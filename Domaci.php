080913cca0720b2183abdd5e19f04c4ef39f4ade
MilosJovic23 <milos.mj992@gmail.com>
2024-10-26 10:59:22 +0200

    * homework assignment

---
+++ Domaci.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+
+
+
+    $siteTitle = "Postani Programer"
+
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo $siteTitle ?></title>
+</head>
+<body>
+    <h1><?php echo $siteTitle ?></h1>
+<nav>
+
+</nav>
+</body>
+</html>
