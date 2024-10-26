4d8b8a628f3c33e9a5c1e818573edfbeb4c966c5
MilosJovic23 <milos.mj992@gmail.com>
2024-10-26 11:05:35 +0200

    * homework assignment

--- Domaci.php
+++ Domaci.php
@@ -2,26 +2,30 @@
 
 
 
-
-
-
-    $siteTitle = "Postani Programer"
-
+    $siteTitle = "Postani Programer";
+    $navItems = ["Glavna","O nama","kontakt"];
 
 
 ?>
 
 <!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php echo $siteTitle ?></title>
-</head>
-<body>
-    <h1><?php echo $siteTitle ?></h1>
-<nav>
 
-</nav>
-</body>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?php echo $siteTitle ?></title>
+    </head>
+    <body>
+        <h1><?php echo $siteTitle ?></h1>
+        <nav>
+            <ul>
+                <a><?php echo $navItems[0] ?></a>
+                <a><?php echo $navItems[1] ?></a>
+                <a><?php echo $navItems[2] ?></a>
+            </ul>
+        </nav>
+    </body>
 </html>
+
+
